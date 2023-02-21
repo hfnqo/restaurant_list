36e34a9cce4364a12cdfb0bef0e576fe26374737
hfnqo <joushitsu22@gmail.com>
2023-02-21 21:07:09 +0800

    feat: add search function

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList })
 })
 
+app.get('/search', (req, res) => {
+  if (!req.query.keyword.trim()) {
+    return res.redirect('/')
+  }
+
+  const keyword = req.query.keyword.trim()
+  let filterRestaurants = restaurantList.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.category.includes(keyword)
+  )
+
+  let searchResult = ''
+  if (filterRestaurants.length <= 0) {
+    searchResult = `找不到符合搜尋字詞「${keyword}」`
+  } else {
+    searchResult = `找到 ${filterRestaurants.length} 筆 符合搜尋字詞「${keyword}」`
+  }
+
+  res.render('index', { restaurants: filterRestaurants, keyword: keyword, searchResult: searchResult })
+})
+
 app.get('/restaurants/:restaurantId', (req, res) => {
   const restaurantId = req.params.restaurantId
   const restaurant = restaurantList.find(restaurant =>
