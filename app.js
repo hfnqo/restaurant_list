10d625f1530bc55ee80c7e8b528901732b5e958e
hfnqo <joushitsu22@gmail.com>
2023-02-21 20:48:39 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const restaurantList = require('./restaurant.json').results
 
 const app = express()
 const port = 3000
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList })
 })
 
 app.listen(port, () => {
