6480c95b7d14ecb097a59cb413dec392c3d39e25
hfnqo <joushitsu22@gmail.com>
2023-02-22 17:33:00 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json').results
+
+// 僅在非正式環境時，使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URL)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('running restaurantSeeder script...')
+
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log('restaurantSeeder done!')
+      db.close()
+    })
+    .catch(error => console.log(error))
+})
