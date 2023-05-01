1f56d9e132480b38c88e07ea20e15fcd2c2efdb0
hfnqo <joushitsu22@gmail.com>
2023-05-01 18:27:35 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
@@ -9,6 +10,12 @@ const port = 3000
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
