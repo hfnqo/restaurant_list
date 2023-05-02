8d283f898835aa627d56f37205720ad747d40412
hfnqo <joushitsu22@gmail.com>
2023-05-02 15:49:45 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const usePassport = require('./config/passport')
 
 require('./config/mongoose')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -24,8 +24,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
 })
