e75d042082208609ba8ec696082fb34c71e7e1fd
hfnqo <joushitsu22@gmail.com>
2023-05-02 11:14:41 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 
+const usePassport = require('./config/passport')
+
 require('./config/mongoose')
 const app = express()
 const port = 3000
@@ -19,6 +21,9 @@ app.use(session({
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 const User = require('../../models/user')
 
@@ -7,8 +8,10 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -17,18 +20,19 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
 
-  User.findOne({ email })
-    .then(user => {
+  User.findOne({ email }).then(user => {
       if (user) {
         console.log('User already exists.')
         res.render('register', { 
           name, email, password, confirmPassword
         })
-      }
-
-      User.create({ name, email, password })
+      } else {
+        return User.create({ 
+          name, email, password 
+        })
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
+      }
     })
     .catch(err => console.log(err))
 })
