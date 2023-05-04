ffea1d7aeb38d967c29ce15568a5139705426aec
hfnqo <joushitsu22@gmail.com>
2023-05-04 12:54:44 +0800

    modifyrestaurantSeeder

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const usePassport = require('./config/passport')
 
 require('./config/mongoose')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
